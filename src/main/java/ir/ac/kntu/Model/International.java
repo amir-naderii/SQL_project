a5f2c84512b4fa318cb837be76df7e2e3a0d587e
amirn <amirnaderi3059@yahoo.com>
2022-01-04 12:18:11 +0330

    Bus Reservation dao completed

--- src/main/java/ir/ac/kntu/Model/International.java
+++ src/main/java/ir/ac/kntu/Model/International.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class International extends Flight{
     public International(Integer id, String departure_city, String departure_airport, String destination_city,
-                         String destination_airport, Date travel_date) {
+                         String destination_airport, Date travel_date, int capacity) {
         super(id, departure_city, departure_airport, destination_city, destination_airport, travel_date, capacity);
     }
 }

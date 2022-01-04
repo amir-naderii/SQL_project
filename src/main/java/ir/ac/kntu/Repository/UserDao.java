e4810a2de2a2dbaf9cdfe318ea236704bb643766
TanazGH <tanaz.ghahremani@gmail.com>
2022-01-04 15:18:52 +0330

    UserInfo & User Updated

--- src/main/java/ir/ac/kntu/Repository/UserDao.java
+++ src/main/java/ir/ac/kntu/Repository/UserDao.java
@@ -1,34 +1,163 @@
 package ir.ac.kntu.Repository;
 
+import ir.ac.kntu.Model.Hotel_Ticket;
 import ir.ac.kntu.Model.User;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 public class UserDao implements Repository<User,Integer> {
+    private final HashMap<String, PreparedStatement> sqlStm = new HashMap<>();
 
-    public User findById(Integer integer) {
+    public UserDao(Connection con){
+        sqlStatements(con);
+    }
+
+    private void sqlStatements(Connection connection) {
+        try {
+            sqlStm.put("findByID",connection.prepareStatement(
+                    "SELECT U.* FROM [User] U where U.id = ?"
+            ));
+            sqlStm.put("findAll",connection.prepareStatement(
+                    "SELECT U.* FROM [User] U"
+            ));
+            sqlStm.put("deleteByID",connection.prepareStatement(
+                    "DELETE FROM [User] WHERE id = ?"
+            ));
+            sqlStm.put("insert",connection.prepareStatement(
+                    "INSERT INTO [User] VALUES(?,?,?,?,?,?)"
+            ));
+            sqlStm.put("update",connection.prepareStatement(
+                    "UPDATE [User] SET credit = ?, user_info_id = ?, creation_date = ?," +
+                            "modification_date = ?, modification_reason = ? WHERE id = ?"
+            ));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private User extractFromResultSet(ResultSet Rs){
+        try {
+            return new User(Rs.getInt(1), Rs.getInt(2),
+                    Rs.getString(3), Rs.getDate(4));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    public List<User> findByIDs(Collection ids) {
+    @Override
+    public User findById(Integer ID) {
+        try{
+            PreparedStatement stmt = sqlStm.get("findByID");
+            stmt.setInt(1,ID);
+            ResultSet Rs = stmt.executeQuery();
+            if(Rs.next()){
+                return extractFromResultSet(Rs);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
+    @Override
+    public List<User> findByIDs(Collection<Integer> IDs) {
+        try {
+            PreparedStatement stmt = sqlStm.get("findByID");
+            ArrayList<User> users = new ArrayList<>();
+            for (Integer i : IDs) {
+                stmt.setInt(1, i);
+                ResultSet Rs = stmt.executeQuery();
+                if(Rs.next()) {
+                    User user = extractFromResultSet(Rs);
+                    users.add(user);
+                }
+            }
+            return users;
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
     public List<User> findAll() {
+        try {
+            PreparedStatement stmt = sqlStm.get("findAll");
+            ResultSet Rs = stmt.executeQuery();
+            ArrayList<User> users = new ArrayList<>();
+            while (Rs.next()){
+                User user = extractFromResultSet(Rs);
+                users.add(user);
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    public Boolean deleteByID(Integer integer) {
-        return null;
+    @Override
+    public Boolean deleteByID(Integer ID) {
+        try{
+            PreparedStatement stmt = sqlStm.get("deleteByID");
+            stmt.setInt(1,ID);
+            int result = stmt.executeUpdate();
+            if(result == 1)
+                return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
-    public Boolean DeleteByIDs(Collection<Integer> integers) {
-        return null;
+    @Override
+    public Boolean DeleteByIDs(Collection<Integer> IDs) {
+        try {
+            PreparedStatement stmt = sqlStm.get("deleteByID");
+            for (Integer i:IDs) {
+                stmt.setInt(1,i);
+                int result = stmt.executeUpdate();
+                if(result != 1)
+                    return false;
+            }
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
+    @Override
     public User save(User E) {
+        try {
+            if(findById(E.getId()) == null){
+                PreparedStatement stmt = sqlStm.get("insert");
+                stmt.setInt(1,E.getId());
+                stmt.setInt(2,E.getCredit());
+                stmt.setString(3,E.getUser_info_id());
+                stmt.setDate(4,(java.sql.Date) E.getCreation_date());
+                stmt.setDate(5, (java.sql.Date) E.getModification_date());
+                stmt.setString(6, E.getModification_reason());
+                return E;
+            }else{
+                PreparedStatement stmt = sqlStm.get("update");
+                stmt.setInt(1,E.getCredit());
+                stmt.setString(2,E.getUser_info_id());
+                stmt.setDate(3,(java.sql.Date) E.getCreation_date());
+                stmt.setDate(4, (java.sql.Date) E.getModification_date());
+                stmt.setString(5, E.getModification_reason());
+                stmt.setInt(6,E.getId());
+                return E;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
--- src/main/java/ir/ac/kntu/Repository/User_InfoDao.java
+++ src/main/java/ir/ac/kntu/Repository/User_InfoDao.java
@@ -1,34 +1,162 @@
 package ir.ac.kntu.Repository;
 
+import ir.ac.kntu.Model.Hotel;
+import ir.ac.kntu.Model.Hotel_type;
+import ir.ac.kntu.Model.User;
 import ir.ac.kntu.Model.User_Info;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
-public class User_InfoDao implements Repository<User_Info, Integer> {
+public class User_InfoDao implements Repository<User_Info, String> {
+    private final HashMap<String, PreparedStatement> sqlStm = new HashMap<>();
 
-    public User_Info findById(Integer integer) {
+    public User_InfoDao(Connection con){
+        sqlStatements(con);
+    }
+
+    private void sqlStatements(Connection connection) {
+        try {
+            sqlStm.put("findByID",connection.prepareStatement(
+                    "SELECT UI.* FROM User_Info UI where UI.id_number = ?"
+            ));
+            sqlStm.put("findAll",connection.prepareStatement(
+                    "SELECT User_Info.* FROM User_Info"
+            ));
+            sqlStm.put("deleteByID",connection.prepareStatement(
+                    "DELETE FROM User_Info WHERE id_number = ?"
+            ));
+            sqlStm.put("insert",connection.prepareStatement(
+                    "INSERT INTO User_Info VALUES(?,?,?,?,?,?,?)"
+            ));
+            sqlStm.put("update",connection.prepareStatement(
+                    "UPDATE User_Info SET email = ?, phone_number = ?, full_name" +
+                            ", gender = ?, birth_date = ?, Pass WHERE id_number = ?"
+            ));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private User_Info extractFromResultSet(ResultSet Rs){
+        try {
+            return new User_Info(Rs.getString(1), Rs.getString(2),
+                    Rs.getString(3),Rs.getString(4), Rs.getString(5),
+                    Rs.getDate(6), Rs.getString(7));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    public List<User_Info> findByIDs(Collection ids) {
+    public User_Info findById(String ID) {
+        try{
+            PreparedStatement stmt = sqlStm.get("findByID");
+            stmt.setString(1,ID);
+            ResultSet Rs = stmt.executeQuery();
+            if(Rs.next()){
+                return extractFromResultSet(Rs);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<User_Info> findByIDs(Collection<String> IDs) {
+        try {
+            PreparedStatement stmt = sqlStm.get("findByID");
+            ArrayList<User_Info> user_infos = new ArrayList<>();
+            for (String i : IDs) {
+                stmt.setString(1, i);
+                ResultSet Rs = stmt.executeQuery();
+                if(Rs.next()) {
+                    User_Info user_info = extractFromResultSet(Rs);
+                    user_infos.add(user_info);
+                }
+            }
+            return user_infos;
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public List<User_Info> findAll() {
+        try {
+            PreparedStatement stmt = sqlStm.get("findAll");
+            ResultSet Rs = stmt.executeQuery();
+            ArrayList<User_Info> user_infos = new ArrayList<>();
+            while (Rs.next()){
+                User_Info user_info = extractFromResultSet(Rs);
+                user_infos.add(user_info);
+            }
+            return user_infos;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    public Boolean deleteByID(Integer integer) {
-        return null;
+    public Boolean deleteByID(String ID) {
+        try{
+            PreparedStatement stmt = sqlStm.get("deleteByID");
+            stmt.setString(1,ID);
+            int result = stmt.executeUpdate();
+            if(result == 1)
+                return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
-    public Boolean DeleteByIDs(Collection<Integer> integers) {
-        return null;
+    public Boolean DeleteByIDs(Collection<String> IDs) {
+        try {
+            PreparedStatement stmt = sqlStm.get("deleteByID");
+            for (String i:IDs) {
+                stmt.setString(1,i);
+                int result = stmt.executeUpdate();
+                if(result != 1)
+                    return false;
+            }
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public User_Info save(User_Info E) {
+        try {
+            if(findById(E.getId_number()) == null){
+                PreparedStatement stmt = sqlStm.get("insert");
+                stmt.setString(1,E.getId_number());
+                stmt.setString(2,E.getEmail());
+                stmt.setString(3,E.getPhone_number());
+                stmt.setString(4,E.getFull_name());
+                stmt.setString(5,E.getGender());
+                stmt.setDate(6, (java.sql.Date)E.getBirth_date());
+                stmt.setString(7, E.getPass());
+                return E;
+            }else{
+                PreparedStatement stmt = sqlStm.get("update");
+                stmt.setString(1,E.getEmail());
+                stmt.setString(2,E.getPhone_number());
+                stmt.setString(3,E.getFull_name());
+                stmt.setString(4,E.getGender());
+                stmt.setDate(5, (java.sql.Date)E.getBirth_date());
+                stmt.setString(6, E.getPass());
+                stmt.setString(7, E.getId_number());
+                return E;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
